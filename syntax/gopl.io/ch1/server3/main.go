3dacc4ad6af0481c807c6ff94e1e7f8dbab36a26
WJX2001 <1758807220@qq.com>
2024-09-15 20:28:47 +0800

    feat: 简易go web服务器

---
+++ syntax/gopl.io/ch1/server3/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Proto：标识HTTP版本 HTTP/1.1
+	fmt.Fprintf(w, "哈哈 %s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	// 将字符串输出到指定的地方，这里是指输出到HTTP响应流 w
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
