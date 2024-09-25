1064637f0af2f8547fcf6e558687f085c65fd963
WJX2001 <1758807220@qq.com>
2024-09-25 15:14:39 +0800

    feat:  JSON使用 请求github

--- syntax/gopl.io/8.JSON/github.go
+++ syntax/gopl.io/8.JSON/github.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+
 	"fmt"
 	"net/http"
 	"net/url"
@@ -52,4 +53,37 @@ func SearchIssues(terms []string) (*IssuesSearchResult, error) {
 	resp.Body.Close()
 	return &result, nil
 
+}
+
+// TODO: 根据问题的时间进行分类，比如不到一个月的、不到一年的、超过一年
+
+// 处理数据
+func Process(data *IssuesSearchResult) {
+	// 创建一个hash表
+	hash := make(map[string][]*Issue, 3) // 映射值是一个指向Issue类型的切片, 初始容量为3
+	hash["不到一月"] = make([]*Issue, 0)
+	hash["不到一年"] = make([]*Issue, 0)
+	hash["超过一年"] = make([]*Issue, 0)
+
+	// 遍历数据
+	for _, val := range data.Items {
+		// 计算从创建时间到现在的持续时间
+		duration := time.Since(val.CreatedAt)
+		if duration.Hours() < 24*30 { // 假设每月30天
+			hash["不到一月"] = append(hash["不到一月"], val)
+		} else if duration.Hours() < 24*365 { // 假设每年365天
+			hash["不到一年"] = append(hash["不到一年"], val)
+		} else {
+			hash["超过一年"] = append(hash["超过一年"], val)
+		}
+	}
+
+	fmt.Printf("数据总条数，%v\n", data.TotalCount)
+	for key, val := range hash {
+		fmt.Printf("%s:\n", key)
+		for _, v := range val {
+			fmt.Printf("#%-5d %9.9s %.55s %9.9v\n",
+				v.Number, v.User.Login, v.Title, v.CreatedAt)
+		}
+	}
 }
--- syntax/gopl.io/8.JSON/main.go
+++ syntax/gopl.io/8.JSON/main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	// MarshalingDemo()
-	githubSearchDemoTest()
+	// githubSearchDemoTest()
+	// queryCount()
+	queryComic()
 }
 
 func githubSearchDemoTest() {
@@ -22,4 +25,24 @@ func githubSearchDemoTest() {
 		fmt.Printf("#%-5d %9.9s %.55s\n",
 			item.Number, item.User.Login, item.Title)
 	}
+}
+
+func queryCount() {
+	query := os.Args[1:]
+	data, err := SearchIssues(query)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	Process(data)
+}
+
+// 对漫画服务进行查询
+func queryComic() {
+	// 获取离线列表
+	// GetSaveInfo()
+	key := os.Args[1:]
+	ResLink(strings.Join(key, " "))
 }
