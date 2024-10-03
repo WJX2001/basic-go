0bd1f2f03c1fb1a43a9c04660cc373312717c2b5
WJX2001 <1758807220@qq.com>
2024-10-03 20:01:26 +0800

    feat: gin 路由

--- syntax/gin-study/main.go
+++ syntax/gin-study/main.go
@@ -18,5 +18,23 @@ func main() {
 		c.String(http.StatusOK, "hello go post")
 	})
 
+	// 参数路由
+	server.GET("/user/:name", func(ctx *gin.Context) {
+		name := ctx.Param("name")
+		ctx.String(http.StatusOK, "hello 这是参数路由"+name)
+	})
+
+	// 通配符路由
+	server.GET("/views/*.html", func(ctx *gin.Context) {
+		page := ctx.Param(".html")
+		ctx.String(http.StatusOK, "hello 这是通配符路由"+page)
+	})
+
+	// 查询参数
+	server.GET("/order", func(ctx *gin.Context) {
+		oid := ctx.Query("id")
+		ctx.String(http.StatusOK, "hello 这是查询参数"+oid)
+	})
+
 	server.Run("localhost:8080")
 }
